89fb5940e22960dab511b9546e8e3483e6f33440
Imikan10 <basseyimikan10@gmail.com>
2024-02-05 12:02:29 +0100

    using props

--- src/index.js
+++ src/index.js
@@ -1,33 +1,47 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
 //CSS
-import './index.css';
+import './index.css'
 
-//SETUP VARS
-  const title = "Happy Valentine's Day, Mouse!"
-  const author = 'Laura Numeroff'
-  const img = 'https://images-na.ssl-images-amazon.com/images/I/81GuYRxyGEL._AC_UL300_SR300,200_.jpg'
+//BOOkS
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81GuYRxyGEL._AC_UL600_SR600,400_.jpg',
+  title: "Happy Valentine's Day, Mouse!",
+  author: 'Laura Numeroff',
+}
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL600_SR600,400_.jpg',
+  title: 'The Housemaid',
+  author: 'Freida McFadden',
+}
 
-function Booklist() {
+function BookList() {
   return (
     <section className='booklist'>
-      <Book job='developer'/>
-      <Book title='random title' number={22}/>
+      <Book 
+        img={firstBook.img} 
+        title={firstBook.title} 
+        author={firstBook.author}
+      />
+      <Book 
+        img={secondBook.img} 
+        title={secondBook.title}
+        author={secondBook.author}
+       />
     </section>
   )
 }
 
 const Book = (props) => {
-  console.log(props);
   return (
     <article className='book'>
-      <img src={img} alt=''/>
-      <h2>{title}</h2>
-      <h4>{author}</h4>
+      <img className='img' src={props.img} alt='' />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   )
 }
 
 // ReactDom.render(<Booklist />, document.getElementById('root'));
-createRoot(document.getElementById('root')).render(<Booklist />)
+createRoot(document.getElementById('root')).render(<BookList />)
