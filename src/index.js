90d8d582ff59540f626fc90930d8878241411cb2
Imikan10 <basseyimikan10@gmail.com>
2024-02-05 13:43:27 +0100

    mapping through arrays

--- src/index.js
+++ src/index.js
@@ -5,61 +5,44 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 //BOOkS
-
-const firstBook = {
+const Books = [
+{
   img: 'https://images-na.ssl-images-amazon.com/images/I/81GuYRxyGEL._AC_UL600_SR600,400_.jpg',
   title: "Happy Valentine's Day, Mouse!",
   author: 'Laura Numeroff',
-}
-const secondBook = {
+},
+{
   img: 'https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL600_SR600,400_.jpg',
   title: 'The Housemaid',
   author: 'Freida McFadden',
-}
-const thirdBook = {
+},
+{
   img: 'https://images-na.ssl-images-amazon.com/images/I/817-Vrzp+tL._AC_UL600_SR600,400_.jpg',
   title: "Little Blue Truck's Valentine",
   author: 'Alice Schertle',
-}
-const fourthBook = {
+},
+{
   img: 'https://images-na.ssl-images-amazon.com/images/I/81uMF1t16LL._AC_UL600_SR600,400_.jpg',
   title: "If Only I Had Told Her",
   author: 'Laura Nowlin',
 }
+]
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book 
-        img={firstBook.img} 
-        title={firstBook.title} 
-        author={firstBook.author}
-      >
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi facilis rerum, id officia debitis minima delectus odit illo accusantium sunt?
-        </p>
-      </Book>
-      <Book 
-        img={secondBook.img} 
-        title={secondBook.title}
-        author={secondBook.author}
-       />
-       <Book 
-        img={thirdBook.img}
-        title={thirdBook.title}
-        author={thirdBook.author}
-       />
-       <Book 
-        img={fourthBook.img}
-        title={fourthBook.title}
-        author={fourthBook.author}
-       />
+      {Books.map((book) => {
+        const {img, title, author} = book;
+        return (
+         <Book book={book}/>
+        )
+      })}
     </section>
   )
 }
 
 const Book = (props) => {
-  const {img, title, author, children} = props
+  const {img, title, author, children} = props.book
   return (
     <article className='book'>
       <img className='img' src={img} alt='' />
